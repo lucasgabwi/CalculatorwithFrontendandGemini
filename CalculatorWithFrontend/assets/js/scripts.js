9db784c353dd9963fb1917f006f08444a7057bb0
Lucas Gabriel Witchemichen <129558303+lucasgabwi@users.noreply.github.com>
2024-08-20 12:22:13 -0300

    Add files via upload

---
+++ CalculatorWithFrontend/assets/js/scripts.js
@@ -0,0 +1,30 @@
+// script.js
+const display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteChar() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function sqrt() {
+    display.value = Math.sqrt(eval(display.value));
+}
+
+function negate() {
+    display.value = -eval(display.value);
+}
